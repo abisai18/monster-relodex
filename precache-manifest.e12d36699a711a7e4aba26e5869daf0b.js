8e0f97b140667defe6f7e2d9fffbaa14c401c88c
Jehiel <ajehiel@gmail.com>
2020-04-12 12:25:41 -0500

    Updates

---
+++ precache-manifest.e12d36699a711a7e4aba26e5869daf0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23fc618f4dab0c5a478ae4167ca07a8",
+    "url": "/monster-relodex/index.html"
+  },
+  {
+    "revision": "e0dff712a8cd8bd3da29",
+    "url": "/monster-relodex/static/css/main.9ea531f3.chunk.css"
+  },
+  {
+    "revision": "d064ea85da7c2e271b4d",
+    "url": "/monster-relodex/static/js/2.d9d4b753.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-relodex/static/js/2.d9d4b753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0dff712a8cd8bd3da29",
+    "url": "/monster-relodex/static/js/main.4fb280e8.chunk.js"
+  },
+  {
+    "revision": "5b3372b4a43355e71b18",
+    "url": "/monster-relodex/static/js/runtime-main.60ac39ff.js"
+  }
+]);
